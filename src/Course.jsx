f6caaf8f2cd5992a0b6932d07852c956e803c8f0
stbam <55661516+stbam@users.noreply.github.com>
2024-06-27 16:44:46 -0400

    efficient solution for 2.5

--- src/Course.jsx
+++ src/Course.jsx
@@ -8,54 +8,27 @@ const Course = (props) => {
         console.log(accumulator +  courses.exercises);
         return accumulator +  courses.exercises;
     },0)
-var sum2=course[1].parts.reduce((accumulator,courses)=>{
-    console.log(accumulator +  courses.exercises);
-    return accumulator +  courses.exercises;
-},0)
+
+
+
 console.log(course[0].name);
 
     return ( 
-    <div>
-        <h1>{course[0].name}</h1>
-        
-       {/*course.map((name,i)=>
-           
-            <h1 key={name.id}>{name.name}</h1>
-    )*/}     
-            
-            
-       {course[0].parts.map(courses=>
-            <p key={courses.id}> 
-                
-                {courses.name} {courses.exercises}  
-            </p>
-        )}
-    <p>The total is {sum}</p>
-
-        <h1>{course[1].name}</h1>
-
-        {course[1].parts.map(courses=>
-            <p key={courses.id}> 
-                
-                {courses.name} {courses.exercises}  
-            </p>
-        )}
-       <p>The total is {sum2}</p>
-
-       {/*
-        const sum = course.parts.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue.exercises;
-    }, 0);
-     */}
-
-
-  
-
-      {/*   <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
-        <p>{props.course.parts[1].name}  {props.course.parts[1].exercises}</p>
-        <p>{props.course.parts[2].name}  {props.course.parts[2].exercises}</p>
-       */ }
-         </div> );
+        <div>
+        {course.map((courseItem) => (
+            <div key={courseItem.id}>
+                <h1>{courseItem.name}</h1>
+                {courseItem.parts.map((part) => (
+                    <p key={part.id}>
+                        {part.name} {part.exercises}
+                    </p>
+                ))}
+                <p>Total exercises: {courseItem.parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
+            </div>
+        ))}
+    </div>
+    
+    );
 }
  
 export default Course;
