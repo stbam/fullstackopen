d5d94328f9f1c1cdf3e20bc8a69159d5dc99d6c3
stbam <55661516+stbam@users.noreply.github.com>
2024-06-27 16:28:41 -0400

    first attempt at maps

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,35 +1,46 @@
 const Course = (props) => {
 
     var sum=0;
+    const {course} = props;
 
- 
-
-const {course} = props;
-console.log(course.parts[0])
-
-console.log(course)
-sum= course.parts.reduce((accumulator,courses)=>{
+    console.log(course[1].parts)
+    sum= course[0].parts.reduce((accumulator,courses)=>{
+        console.log(accumulator +  courses.exercises);
+        return accumulator +  courses.exercises;
+    },0)
+var sum2=course[1].parts.reduce((accumulator,courses)=>{
+    console.log(accumulator +  courses.exercises);
     return accumulator +  courses.exercises;
 },0)
+console.log(course[0].name);
 
-
-
-    return ( <div>
-
-
- 
-
-         <h1>{props.course.name}</h1>
+    return ( 
+    <div>
+        <h1>{course[0].name}</h1>
         
-       {course.parts.map(courses=>
+       {/*course.map((name,i)=>
+           
+            <h1 key={name.id}>{name.name}</h1>
+    )*/}     
+            
+            
+       {course[0].parts.map(courses=>
             <p key={courses.id}> 
-                {/*console.log(courses.exercises)*/}
+                
                 {courses.name} {courses.exercises}  
-
             </p>
         )}
-       
-        
+    <p>The total is {sum}</p>
+
+        <h1>{course[1].name}</h1>
+
+        {course[1].parts.map(courses=>
+            <p key={courses.id}> 
+                
+                {courses.name} {courses.exercises}  
+            </p>
+        )}
+       <p>The total is {sum2}</p>
 
        {/*
         const sum = course.parts.reduce((accumulator, currentValue) => {
@@ -38,7 +49,7 @@ sum= course.parts.reduce((accumulator,courses)=>{
      */}
 
 
-    <p>The total is {sum}</p>
+  
 
       {/*   <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
         <p>{props.course.parts[1].name}  {props.course.parts[1].exercises}</p>
