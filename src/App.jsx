323075564f5c2db28a172e138eac6c8031f5e40c
stbam <55661516+stbam@users.noreply.github.com>
2024-06-28 00:37:33 -0400

    2.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes,setNotes] = useState(props.notes)
+  const [newNote,setNewNote] = useState(
+    'a new note'
+  )
+  const[showAll,setShowAll]=useState(true);
+  const notesToShow = showAll
+  ?notes
+  :notes.filter(note=>note.important===true) 
+
+
+  const addNote = (event)=>{
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < .5,
+      id:notes.length+1
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange= (event) =>{
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  }
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
+        {notesToShow.map(note=>
+          <Note key={note.id} note={note}/>
+          )}
+      </ul>
+          <div>
+            <button onClick={()=>setShowAll(!showAll)}>
+              {console.log(showAll)}
+              show {showAll ? 'important':'all'}
+            </button>
+          </div>
+
+      <ul>
+        
         {notes.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+
+        <input 
+        value={newNote}
+        onChange={handleNoteChange}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+
     </div>
   )
 }
